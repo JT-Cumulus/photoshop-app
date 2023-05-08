f97cdf412f02f029e7c70b53bcee403f28cf233c
jt-io <jiatong.cui@calco.nl>
2023-05-08 14:48:28 +0200

    prep files for customer interaction

--- repository/Customer.java
+++ repository/Customer.java
@@ -1,6 +1,12 @@
 package repository;
 
-public abstract class Customer {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Customer {
 
     private int ID;
     private String firstName;
@@ -9,10 +15,54 @@ public abstract class Customer {
     private String postcode;
     private String city;
     private String email;
-    private int mobile;
+    private String mobile;
 
-    public Customer(int id) {
-        this.ID = id;
+
+    public Customer(int ID, String firstName, String lastName, String address, String postcode, String city, String email, String mobile) {
+        this.ID = ID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.postcode = postcode;
+        this.city = city;
+        this.email = email;
+        this.mobile = mobile;
+    }
+
+    private static Customer createCustomer(String[] metadata) {
+        int id = Integer.parseInt(metadata[0]);
+        String firstName = metadata[1];
+        String lastName = metadata[2];
+        String address = metadata[3];
+        String postcode = metadata[4];
+        String city = metadata[5];
+        String email = metadata[6];
+        String mobile = metadata[7];
+
+        return new Customer(id, firstName, lastName, address, postcode, city, email, mobile);
+    }
+
+    public static ArrayList<Customer> loadCustomers(){
+        String fileName = "user/PhotoShop_Customers.csv";
+        File file = new File(fileName);
+        ArrayList<Customer> customerContainer = new ArrayList<>();
+        // this gives you a 2-dimensional array of strings
+        Scanner inputStream;
+
+        try{
+            inputStream = new Scanner(file);
+            while(inputStream.hasNext()){
+                String line = inputStream.nextLine();
+                String[] values = line.split(";");
+                // this adds the currently parsed line to the 2-dimensional string array
+                Customer customer = createCustomer(values);
+                customerContainer.add(customer);
+            }
+            inputStream.close();
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return customerContainer;
     }
 
     public int getID() {
@@ -71,11 +121,11 @@ public abstract class Customer {
         this.email = email;
     }
 
-    public int getMobile() {
+    public String getMobile() {
         return this.mobile;
     }
 
-    public void setMobile(int mobile) {
+    public void setMobile(String mobile) {
         this.mobile = mobile;
     }
 
