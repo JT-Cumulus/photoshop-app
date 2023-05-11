21436e82ebeeb13fbe6ba6fb76da432eddc7491e
jt-io <jiatong1998@yahoo.com>
2023-05-11 14:51:30 +0200

    implement invoice retriever

--- service/Invoice.java
+++ service/Invoice.java
@@ -2,6 +2,11 @@ package service;
 
 import user.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import repository.Customer;
 import repository.Employee;
 import service.ShoppingCart;
@@ -36,16 +41,37 @@ public class Invoice {
     private Customer soldCustomer;
     private Order soldOrder;
 
-     public Invoice(Employee employee, Customer customer, ShoppingCart shoppingCart){
+     public Invoice(Employee employee, Customer customer, Order order){
         this.soldEmployee = employee;
         this.soldCustomer = customer;
-        this.soldOrder = new Order(shoppingCart);
+        this.soldOrder = order;
      }
      
      public void displayInvoice(int orderID){
 
-        this.soldEmployee.getFirstName();
-
-        this.soldCart.displayCart();
      }
+
+     // Find an order from its id within the invoices TODO
+    public ArrayList<String> findInvoice(int orderID){
+      String fileName = "./invoices/order_" + orderID + ".json";
+      File file = new File(fileName);
+
+      // this gives you a 2-dimensional array of strings
+      Scanner inputStream;
+      ArrayList<String> cob = new ArrayList<>();
+      try{
+          inputStream = new Scanner(file);
+          while(inputStream.hasNext()){
+              String line = inputStream.nextLine();
+              String[] values = line.split(",");
+              // this adds the currently parsed line to the 2-dimensional string array
+              cob.add(values[0]);
+          }
+          inputStream.close();
+          
+      }catch (FileNotFoundException e) {
+          e.printStackTrace();
+      }
+      return cob;
+  }
 }
