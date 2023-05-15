415793fafd017a16c1033bd014d76f4cd2e6ad52
jt-io <jiatong1998@yahoo.com>
2023-05-15 13:34:27 +0200

    update order as extension of cart

--- service/Order.java
+++ service/Order.java
@@ -28,6 +28,7 @@ public class Order {
     }
 
     public Order(){
+        currentOrder = new ArrayList<Item>();
 
     }
 
@@ -42,7 +43,7 @@ public class Order {
 
     // Find an order from its id within the invoices TODO
     public ArrayList<String> getOrder(int orderID){
-        String fileName = "./invoices/order_" + orderID + ".json";
+        String fileName = "./database/PhotoShop_Orders.csv";
         File file = new File(fileName);
   
         // this gives you a 2-dimensional array of strings
@@ -53,9 +54,11 @@ public class Order {
 
             while(inputStream.hasNext()){
                 String line = inputStream.nextLine();
-                String[] values = line.split(",");
+                String[] values = line.split(";");
                 // this adds the currently parsed line to the 2-dimensional string array
-                cob.add(values[0]);
+                if(Integer.parseInt(values[0]) == orderID){
+                    System.out.println(values);
+                }
             }
             inputStream.close();
             
@@ -65,22 +68,86 @@ public class Order {
         return cob;
     }
 
-    public void retrieveOrder(ArrayList<String> arrayList, Catalogue catalogue){
-        this.orderID = Integer.parseInt(arrayList.get(0));
-        List<String> container = Arrays.asList(arrayList.get(1).split(";"));
-        List<Item> temp = new ArrayList<Item>();
-        for (String line : container){
-            List<String> indexAndQuantity = Arrays.asList(line.split(":"));
-            int index = Integer.parseInt(indexAndQuantity.get(0));
-            int quantity = Integer.parseInt(indexAndQuantity.get(1));
-            temp.add(catalogue.getItem(index));
-            temp.get(index).setQuantity(quantity);
+    public void displayCart(){
+        System.out.println(String.format("%-5s %-30s %15s %20s %10s" , "ID", "Item", "Price(EUR)", "Time to Make (min)", "Quantity"));
+        for (Item object: this.currentOrder) {
+            this.totalPrice += object.getPrice() * object.getQuantity();
+            this.totalTimeTaken += object.getMinutes() * object.getQuantity();
+            System.out.println(object + "\t   " + object.getQuantity());
         }
-        this.currentOrder = temp;
-        this.totalPrice = Double.parseDouble(arrayList.get(2));
-        this.totalTimeTaken = Long.parseLong(arrayList.get(3));
-        this.pickupDate = LocalDate.parse(arrayList.get(4));
-
+        System.out.println("Total Price: " + this.getTotalPrice() + " EUR");
+        System.out.println("Total Time Required: " + ((this.totalTimeTaken / 60) + " working hours"));
     }
+
+    // Return the item within the cart such that the quantity can be updated in the shopping cart
+    public Item getIteminCart(Item item){
+        List<Item> temp = this.currentOrder;
+        for (Item object: temp) {
+            if (object.getId() == item.getId()){
+                return object;
+            } 
+        }
+        return null;
+    }
+
+    // Add item to the shopping cart
+    public void addItem(Item item){
+        if(getIteminCart(item) != null) {
+            getIteminCart(item).addQuantity();
+        } else {
+            this.currentOrder.add(item);
+        }        
+    }
+
+    // Remove item from shopping cart
+    public void removeItem(Item item){
+        if(getIteminCart(item) != null) {
+            getIteminCart(item).addQuantity();
+        } else {
+            this.currentOrder.remove(item);
+        }
+    }
+
+
+    public List<Item> getCurrentOrder() {
+        return this.currentOrder;
+    }
+
+    public void setCurrentOrder(List<Item> currentOrder) {
+        this.currentOrder = currentOrder;
+    }
+
+    public Integer getOrderID() {
+        return this.orderID;
+    }
+
+    public void setOrderID(Integer orderID) {
+        this.orderID = orderID;
+    }
+
+    public double getTotalPrice() {
+        return this.totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public long getTotalTimeTaken() {
+        return this.totalTimeTaken;
+    }
+
+    public void setTotalTimeTaken(long totalTimeTaken) {
+        this.totalTimeTaken = totalTimeTaken;
+    }
+
+    public LocalDate getPickupDate() {
+        return this.pickupDate;
+    }
+
+    public void setPickupDate(LocalDate pickupDate) {
+        this.pickupDate = pickupDate;
+    }
+
     
 }
